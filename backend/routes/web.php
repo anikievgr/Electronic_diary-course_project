9e1f56b2987b266c9d2e08995a5fdc9945264ea2
anikievgr <anikiev.gleb.2005@yandex.ru>
2023-05-20 17:49:22 +0300

    закончил с формой регистрации

--- backend/routes/web.php
+++ backend/routes/web.php
@@ -14,15 +14,16 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/', function () {
-    return view('welcome');
-});
 
-Route::get('/dashboard', function () {
+
+Route::get('/test', function () {
     return view('dashboard');
 })->middleware(['auth', 'verified'])->name('dashboard');
 
 Route::middleware('auth')->group(function () {
+    Route::get('/', function (){
+        echo 12;
+    });
     Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
     Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
     Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
