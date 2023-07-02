f2e73225ca7beb33103c6318342a1b4f0d36e81f
anikievgr <anikiev.gleb.2005@yandex.ru>
2023-07-02 16:02:34 +0300

    conflict

--- backend/routes/web.php
+++ backend/routes/web.php
@@ -51,7 +51,7 @@ Route::middleware('auth')->group(function () {
 
     Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
     Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
-    Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');//test
+    Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');//conflict
 });
 
 require __DIR__.'/auth.php';
